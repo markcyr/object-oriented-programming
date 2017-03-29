a8d5dfad469ffe6b68aa7a469520b4bf3dbf1b02
markcyr <markcyr@msn.com>
2017-03-29 17:44:21 -0400

    Cats exercise complete

--- Cat.rb
+++ Cat.rb
@@ -1,12 +1,25 @@
 class Cat
 
+  attr_accessor  :preferred_food, :meal_time,  :food, :meal, :eats_at, :eats, :catsname
 
-
-  def initialize(name,preferred_food,meal_time)
+  def initialize(name, preferred_food, meal_time)
     @catsname = name
     @food = preferred_food
     @meal = meal_time
   end
 
+  def eats_at
+    if @meal < 12
+    eats =  "#{@catsname}'s meal time is #{@meal} AM"
 
+    else
+      eats = "#{@catsname}'s meal time is #{@meal - 12} PM"
+
+    end
+  end
+
+  def meow
+    puts "My name is #{@catsname} and I eat #{@food} at #{eats_at}"
+
+  end
 end
