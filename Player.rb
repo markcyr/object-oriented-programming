24888e70529fedb99a507e4a226e9b27ddeff9a9
markcyr <markcyr@msn.com>
2017-03-29 19:15:03 -0400

    Player with working code

--- Player.rb
+++ Player.rb
@@ -1,32 +1,46 @@
 class Player
 
-lives =  5
-gold_coins=  0
-health_points =  10
+  attr_writer :damage
+  attr_reader :health_points, :gold_coins
+
+  def initialize
+    @lives =  5
+    @gold_coins=  0
+    @health_points =  10
+  end
 
   def level_up
-    lives = lives + 1
+    @lives += 1
   end
 
   def collect_treasure
-    if gold_coins % 10
+    @gold_coins  += 1
+    if @gold_coins % 10 == 0
     level_up
+    end
   end
 
   def do_battle(damage)
-    health_points = health_points - damage
-    if health_points < 1
-      lives = lives - 1
-      if lives < 1
-        reset.call
+    @health_points = @health_points - damage
+#      puts "#{health_points} #{@lives}"
+    if @health_points < 1
+#        puts "#{@health_points} #{@lives}"
+      @lives = @lives - 1
+      if @lives < 1
+          reset
+#          puts "hi"
       else
-        health_points = 10
+        @health_points = 10
+      end
     end
-    def reset
-      lives =  5
-      gold_coins=  0
-      health_points =  10      
-    end
+  end
+
+
+  def reset
+   @lives = 5
+   @gold_coins= 0
+   @health_points = 10
+  end
 
 
 end
