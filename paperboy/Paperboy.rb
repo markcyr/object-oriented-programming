c1e001ca649758ec60bdebcf3819acf32002ccd3
markcyr <markcyr@msn.com>
2017-03-29 19:39:10 -0400

    Starting Paperboy route

---
+++ paperboy/Paperboy.rb
@@ -0,0 +1,14 @@
+class Paperboy
+
+  attr_writer :name, :experience, :earnings
+  attr_accessor :name
+
+  
+  def initialize(name)
+    @name =  name
+#    @experience =  number_of_papers
+#    @earnings =  money
+  end
+
+
+end
