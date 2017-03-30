0832f80defa513c77b0b8920d66e4e12d07ba868
markcyr <markcyr@msn.com>
2017-03-29 23:03:23 -0400

    Paperboy done

--- paperboy/Paperboy.rb
+++ paperboy/Paperboy.rb
@@ -1,36 +1,45 @@
 class Paperboy
 
   attr_writer :name, :experience, :earnings
-  attr_accessor :name , :start_address, :end_address, :deliver
+  attr_accessor :name , :start_address, :end_address, :total_paper_deliver
 
 
   def initialize(name)
     @name =  name
-    @experience =  50
+    @experience = 50
     @earnings =  0.00
+    @total_paper_deliver = 0
   end
 
   def quota
-    @experience = @experience + (@experience * 0.5)
+    if @experience > 49
+      @experience = @experience + (@experience * 0.5)
+    else
+      @experience = 50
+    end
   end
 
   def deliver(start_address, end_address)
-    total_paper_deliver = end_address - start_address
-      if end_address >= start_address
+    @total_paper_deliver = end_address - start_address
+    puts "#{@total_paper_deliver}"
+      if end_address <= start_address
         puts "What are you doing"
       end
-end
+  end
 
-  def earnings
-    if total_paper_deliver > @experience
-      @earnings = @earnings - 2.00 + (total_paper_deliver * 0.25)
+  def earning
+    if @total_paper_deliver < @experience
+      @earnings = @earnings - 2.00 + (@total_paper_deliver * 0.25)
+      puts "#{@earnings} under"
     else
-      over_quota = (total_paper_deliver - @experience) * 0.50
-      @earnings = @earnings + over_quota + (total_paper_deliver * 0.25)
+      over_quota = (@total_paper_deliver - @experience) * 0.25
+      @earnings = @earnings + over_quota + (@total_paper_deliver * 0.25)
+      puts "#{@earnings} over"
+    end
   end
 
   def report
-    puts "I'm #{@name}, I've delivered #{@experience} papers and I've earned $#{@earnings} so far!"
+    puts "I'm #{@name}, I've delivered #{@total_paper_deliver} papers today and I've earned $#{@earnings} so far!"
   end
 
 end
